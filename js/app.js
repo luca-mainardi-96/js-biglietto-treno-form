a1d59578045c9c196a74b4cd0bbe19d401b6983f
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-25 18:37:20 +0200

    js + correzione html

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
-// Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all’utente il numero di chilometri che vuole percorrere e lo sconto da applicare scegliendo tra 3 opzioni:
-// prezzo intero
-// sconto studente
-// sconto over 65
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// Il recap dei dati e l’output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
-// Consigli
-// Potete recuperare il vecchio codice anche se la logica è leggermente diversa in quanto non avete più l’età del passeggero ma bensi la scelta del tipo di sconto. Copiate pure le formule per calcolare lo sconto ma il mio consiglio è di riscrivere la logica dell’esercizio da capo.
-
-
-
-
-
-// Continuate sull’esercizio di martedi.
-// Aggiungete Bootstrap al progetto sistemando la ui usando i componenti di Bootstrap per il form e magari creare una piccola card dove stampare il prezzo.
-// Bonus:
-// Aggiungete altri campi dove inserire anche nome e cognome del passeggero
-// stampare e vero e proprio biglietto con tutte le info del biglietto. Potete usare la creatività per la grafica e la UI.
-
+//Dichiarazione Variabili
 
 const inputNome = document.getElementById('nome')
 const inputCognome = document.getElementById('cognome')
 const inputTratta = document.getElementById('tratta')
 const selectSconto = document.getElementById('sconto')
-const button = document.getElementById('button')
+const formBiglietto = document.getElementById('form')
 
-console.log(inputNome, inputCognome, inputTratta, selectSconto, button)
+const outputNome = document.getElementById('output-nome')
+const outputCognome = document.getElementById('output-cognome')
+const outputTratta = document.getElementById('output-km')
+const outputSconto = document.getElementById('output-sconto')
+const outputPrezzoFinale = document.getElementById('output-prezzo-finale')
+
+const prezzo = inputTratta*0.21
+let PrezzoFinale = ''
+
+formBiglietto.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    //recuperare valori form
+    const nome = inputNome.value
+    const cognome = inputCognome.value
+    const tratta = inputTratta.value
+    const sconto = selectSconto.value
+
+    //stampare nella card-biglietto
+
+    outputNome.innerHTML = nome
+    outputCognome.innerHTML = cognome
+    outputTratta.innerHTML = tratta
+    outputSconto.innerHTML = sconto
+    outputPrezzoFinale.innerHTML = PrezzoFinale
+
+
+})
+
+
+// Calcolo prezzo finale
+
+if (inputTratta === '20%'){
+    PrezzoFinale = prezzo - (prezzo*0.2)
+} else if (inputTratta === '40%'){
+    PrezzoFinale = prezzo -(prezzo*.04)
+} else {
+    PrezzoFinale = prezzo
+}
