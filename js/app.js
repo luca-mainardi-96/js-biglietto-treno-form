c542a0166e320ff1bc5b4070084a38bd2f250ba5
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-25 13:32:11 +0200

    creazione repo

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all’utente il numero di chilometri che vuole percorrere e lo sconto da applicare scegliendo tra 3 opzioni:
+// prezzo intero
+// sconto studente
+// sconto over 65
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// Il recap dei dati e l’output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+// Consigli
+// Potete recuperare il vecchio codice anche se la logica è leggermente diversa in quanto non avete più l’età del passeggero ma bensi la scelta del tipo di sconto. Copiate pure le formule per calcolare lo sconto ma il mio consiglio è di riscrivere la logica dell’esercizio da capo.
+
+
+
+
+
+// Continuate sull’esercizio di martedi.
+// Aggiungete Bootstrap al progetto sistemando la ui usando i componenti di Bootstrap per il form e magari creare una piccola card dove stampare il prezzo.
+// Bonus:
+// Aggiungete altri campi dove inserire anche nome e cognome del passeggero
+// stampare e vero e proprio biglietto con tutte le info del biglietto. Potete usare la creatività per la grafica e la UI.
+
+
